1c76d2640629fac8297c694047bdf0541ddf3735
lnx-e <lnxegg@gmail.com>
2022-08-23 18:26:05 +0530

    Added Attack and Health

---
+++ not alone/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector3 movementVector;
+    private WeaponParent weaponParent;
+    public GameObject weapon;
+
+    [SerializeField] float speed = 5;
+
+
+
+    public float autoAttackCooldown;
+    public float autoAttackCurTime;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movementVector = new Vector3();
+        weaponParent = weapon.GetComponent<WeaponParent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementVector.x = Input.GetAxisRaw("Horizontal");
+        movementVector.y = Input.GetAxisRaw("Vertical");
+
+        movementVector *= speed;
+
+        rb.velocity = movementVector;
+
+        if (autoAttackCurTime < autoAttackCooldown)
+        {
+            autoAttackCurTime += Time.deltaTime;
+        }
+        else
+        {
+            weaponParent.Attack();
+            autoAttackCurTime = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        difference.Normalize();
+
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+
+    }
+}
---
+++ not alone/Assets/Scripts/WeaponParent.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponParent : MonoBehaviour
+{
+    public GameObject myPlayer;
+
+    public Animator Animator;
+    public float delay = 0.3f;
+    private bool attackBlocked;
+
+    public Transform circleOrigin;
+    public float radius;
+
+    private void FixedUpdate()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        difference.Normalize();
+
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+
+    }
+
+    public void Attack()
+    {
+        Animator.SetTrigger("Attack");    
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Vector3 position = circleOrigin == null ? Vector3.zero : circleOrigin.position;
+        Gizmos.DrawWireSphere(position, radius);
+    }
+
+    public void DetectColliders()
+    {
+        foreach(Collider2D collider in Physics2D.OverlapCircleAll(circleOrigin.position, radius))
+        {
+            //Debug.Log(collider.name);
+            Health health;
+            if (health = collider.GetComponent<Health>())
+            {
+                health.GetHit(1, transform.parent.gameObject);
+            }
+        }
+    }
+}
